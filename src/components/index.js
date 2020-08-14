9304de63151bec6e8521e8b78986602a242c71d6
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-14 19:48:03 -0400

    Facilitate component library tree shaking

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+export { default as Jumbotron } from './jumbotron';
+
+
+// This method of using imports tells
+// webpack that we are using this file
+// since we are exporting it elsewhere
+
+// import Jumbotron from './jumbotron'
+// import Mumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+// import Jumbotron from './jumbotron'
+
+// export {
+//     Jumbotron,
+//     Mumbotron,
+//     More,
+//     More
+
+// }
--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import jumboData from '../fixtures/jumbo.json';
-import Jumbotron from '../components/jumbotron';
+import { Jumbotron } from '../components';
+// import Jumbotron from '../components/jumbotron';
 
 export function JumbotronContainer() {
     return (
