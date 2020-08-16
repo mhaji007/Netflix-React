76e979807bf63326f91f9ee462eef01abafa5e52
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-16 15:02:00 -0400

    Create header

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Jumbotron } from './jumbotron';
 export { default as Footer } from './footer';
 export { default as Accordion } from './accordion';
 export { default as OptForm } from './opt-form';
+export { default as Header } from './header';
 
 
 // This method of using imports tells
