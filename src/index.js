9523811ccd5045f897139ac5fd4d0f2f081ed8d6
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-17 01:37:35 -0400

    Connect app to Firebase

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import { App } from './app';
 import { FirebaseContext } from './context/firebase';
 
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: "AIzaSyBjplLo7-gjsKqx_yb6vhJhSMOsASBtZOE",
+    authDomain: "netflix-react-a95b8.firebaseapp.com",
+    databaseURL: "https://netflix-react-a95b8.firebaseio.com",
+    projectId: "netflix-react-a95b8",
+    storageBucket: "netflix-react-a95b8.appspot.com",
+    messagingSenderId: "64208235344",
+    appId: "1:64208235344:web:ef504c6996a8d7c6d65a55",
+    measurementId: "G-KDKJX9QYGN"
 }
 
 const firebase = window.firebase.initializeApp(config);
