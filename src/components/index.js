1ace840e20d90c0b251e719d9630a190e2a041da
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-17 22:05:45 -0400

    Create profile

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export { default as OptForm } from './opt-form';
 export { default as Header } from './header';
 export { default as Feature } from './feature';
 export { default as Form } from './form';
+export { default as Profiles } from './profiles';
 
 
 // This method of using imports tells
